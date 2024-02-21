c41d95dcbe6197c286450de7a5abd178db145593
VictorRezende <victor.franco_rezende@syngenta.com>
2024-02-21 07:33:30 -0300

    feat: everything done

--- README.md
+++ README.md
@@ -9,3 +9,34 @@
 - Typescript
 
 # How to run this application
+Given that you already have docker installed in your machine, to run this application 
+you will need the following steps:
+
+## 0 - Prepare the ambient:
+    >> npm install
+
+## 1 - Run Docker
+    >> npm run docker
+There will be no data in the database, connect on the docker and run the ~/ddl/models.sql to create
+the tables.
+
+## 2 - Run the program itself
+    >> npm run start
+It's very important to notice that this project is using Node v20.11. Versions under v20 might not work.
+
+## 3 - The bucket s3 feed:
+You need to config your AWS environment to run it. If you have access to the bucket it will run. 
+
+
+# Use of AI tools
+
+Here is the links to my conversation with ChatGPT
+
+https://chat.openai.com/share/45850ecc-9447-4d48-9b15-782b88fa8a6b
+https://chat.openai.com/share/2fa5712d-1917-4040-90fd-7e16b9349936
+
+My approach to utilizing ChatGPT is akin to employing a turbocharged search engine.
+When conventional searches fail to yield the desired results, I turn to the AI for comprehensive insights. 
+The code snippets it provides serve as invaluable foundations for comprehending processes and streamlining 
+repetitive tasks.
+
--- src/services/aws/s3bucket.service.ts
+++ src/services/aws/s3bucket.service.ts
@@ -1,21 +1,51 @@
-import { JobEntity } from "../../job/entity/job.entity";
-import { jobEntityToJobFeedDTO } from "../../job/entity/job-feed.entity";
 import { Response } from "express";
 import { nodeCache } from "../node-cache/node-cache";
+import AWS, { S3 } from "aws-sdk";
+import { JobEntity } from "../../job/entity/job.entity";
+import { jobEntityToJobFeedDTO } from "../../job/entity/job-feed.entity";
 
 class S3bucketService {
+  constructor(public readonly s3: S3) {}
+
+  public async fetchS3FileContents(): Promise<string | null> {
+    try {
+      const params = {
+        Bucket: "job-feed-bucket",
+        Key: "jobs.json",
+      };
+
+      const data = await this.s3.getObject(params).promise();
+      if (data.Body) {
+        return data.Body.toString();
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching file from S3:", error);
+      return null;
+    }
+  }
   public async getFeed(res: Response) {
+    await nodeCache.clear();
     const cachedFeed = await nodeCache.getFeedFromCache();
     if (cachedFeed) {
       return cachedFeed;
     }
-    //TODO FAZER O BUCKET AQUI
-    const jobsEntity: JobEntity[] = require("./jobs.json");
-    const jobsFeed = jobsEntity.map(jobEntityToJobFeedDTO);
-    await nodeCache.saveFeedOnCache(jobsFeed);
+    const jobsEntity = await this.fetchS3FileContents();
+    if (jobsEntity) {
+      const jsonJobs: JobEntity[] = JSON.parse(jobsEntity);
+      const jobsFeed = jsonJobs.map(jobEntityToJobFeedDTO);
+      await nodeCache.saveFeedOnCache(jobsFeed);
+      res.status(200);
+      res.json(jobsFeed);
+    }
     res.status(200);
-    res.json(jobsFeed);
+    res.json();
   }
 }
 
-export const s3BucketService = new S3bucketService();
+export const s3BucketService = new S3bucketService(
+  new AWS.S3({
+    region: "eu-north-1",
+  }),
+);
